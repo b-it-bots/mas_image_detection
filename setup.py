a52309e9eb0dca3a92c21cbf62e2a3c3a23859ee
Minh Nguyen <minhnh.91@gmail.com>
2019-04-10 17:13:46 +0200

    use b-it-bots folder structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['mas_image_detection'],
-    package_dir={'': 'src'}
+    package_dir={'': 'ros/src'}
 )
 
 setup(**d)
